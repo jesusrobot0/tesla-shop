0347824e1f80f6457f525e9095a8ec88f7560d6b
jesusrobot0 <jesusrobot9000@icloud.com>
2023-11-24 16:06:09 -0600

    Añade al ProductGridItem la funcionalidad de cambiar la imagen al hacer hover en la imagen

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,21 +1,28 @@
-import { Product } from "@/interfaces";
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Product } from "@/interfaces";
 
 interface Props {
   product: Product;
 }
 
 export function ProductGridItem({ product }: Props) {
+  const initialImage = product.images[0];
+  const [displayImage, setDisplayImage] = useState(initialImage);
   return (
-    <div className="rounded-md overflow-hidden fade-in">
+    <div className="overflow-hidden fade-in">
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${product.images[0]}`}
+          src={`/products/${displayImage}`}
           alt={product.title}
           className="w-full object-cover"
           width={500}
           height={500}
+          onMouseEnter={() => setDisplayImage(product.images[1])}
+          onMouseLeave={() => setDisplayImage(initialImage)}
         />
       </Link>
       <div className="p-4 flex flex-col">
