6b9c32eae390273ac8e608a5a2565a52f561e025
jesusrobot0 <jesusrobot9000@icloud.com>
2023-11-24 13:03:52 -0600

    Añade rutas de la app

---
+++ src/app/(shop)/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function OrdersPage() {
+  return (
+    <div>
+      <h1>Orders List</h1>
+    </div>
+  );
+}
---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Shop</h1>
+    </div>
+  );
+}
